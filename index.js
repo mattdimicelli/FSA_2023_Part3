b011a0761dc7254b1e52967229af66074508d766
MDiMicelli <mdimicelli@northwell.edu>
2023-09-09 12:18:23 -0400

    finish 3.8

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
-app.use(morgan('tiny'));
+app.use(morgan((tokens, req, res) => {
+    const body = req.body;
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms',
+        JSON.stringify(body),
+    ].join(' ')
+}));
 
 let entries = [
     {
